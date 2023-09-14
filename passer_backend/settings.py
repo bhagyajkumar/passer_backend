dde33c4a2f2a78dc20db7fd7f139e23127cf48eb
bhagyaj <bhagyajkumar1@gmail.com>
2023-09-14 23:02:25 +0530

    update settings

--- passer_backend/settings.py
+++ passer_backend/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://bhagyajkumar.github.io/passer_backend/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
